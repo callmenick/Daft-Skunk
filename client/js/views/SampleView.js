2d7c100ee515cda4b2c3af1e420d291e7448fc40
Nick Salloum <nick@callmenick.com>
2015-08-03 22:28:15 -0700

    initial samples library and samples preview implemented

--- client/js/views/SampleView.js
+++ client/js/views/SampleView.js
@@ -12,13 +12,40 @@ app.SampleView = Backbone.View.extend({
 
   template: Handlebars.compile($('#sample-template').html()),
 
+  events: {
+    'click .sample__preview' : 'previewSample',
+  },
+
   initialize: function() {
     this.render();
+
+    this.$el.find('.sample__audio')[0].addEventListener('ended', function() {
+      this.stopSample();
+    }.bind(this));
   },
 
   render: function() {
     this.$el.html(this.template(this.model.attributes));
     return this;
+  },
+
+  previewSample: function() {
+    if (this.$el.find('.sample__preview').hasClass('fa-play')) {
+      this.playSample();
+    } else {
+      this.stopSample();
+    }
+  },
+
+  playSample: function() {
+    this.$el.find('.sample__preview').removeClass('fa-play').addClass('fa-pause');
+    this.$el.find('.sample__audio')[0].play();
+  },
+
+  stopSample: function() {
+    this.$el.find('.sample__preview').removeClass('fa-pause').addClass('fa-play');
+    this.$el.find('.sample__audio')[0].pause();
+    this.$el.find('.sample__audio')[0].currentTime = 0;
   }
 
 });
